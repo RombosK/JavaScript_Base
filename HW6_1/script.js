701baa22aba7736a9b3f034f647fa2f29e67f3b8
Roman <rombos1978@gmail.com>
2022-04-16 16:27:15 +0300

    HW6

---
+++ HW6_1/script.js
@@ -0,0 +1,134 @@
+// создаем объект каталога
+function Item(product, image, description, price, discount = 0) {
+    this.product = product;
+    this.image = `img/${image}.jpg`;
+    this.description = description;
+    this.price = price;
+    this.discount = discount
+}
+
+let wishList = []
+
+wishList.push(
+    new Item('Samsung-S20', 'Image1', 'made in China', 45000, 10)
+);
+wishList.push(
+    new Item('iPhone-12', 'Image2', 'made in Taiwan', 110000, 5)
+);
+wishList.push(
+    new Item('Xiaomi-Mi9', 'Image3', 'made in Korea', 40000)
+);
+wishList.push(
+    new Item('Vivo-V20', 'Image4', 'made in China', 42000, 20)
+);
+
+
+// создаем отображение каталога
+function drowItems() {
+    wishList.forEach(function (item, i) {
+        drowItem(item, i);
+    })
+}
+
+const $catalog = document.querySelector('#catalog');
+function drowItem(item, id) {
+    $catalog.insertAdjacentHTML('beforeend',
+        `<div id="item-${id}" class="prod_item">
+        <div class="item">
+            <div class="image"><img src="${item.image}"></div>
+            <div class="description"><h4>${item.product}</h4>${item.description}
+                <div class="price">Цена: 
+                    <span>${item.price}</span> руб.
+                </div>
+            </div>
+        </div>
+        <div class="sale">
+            <span class='offer ${item.discount > 0 ? 'show' : ''}'>Скидка: ${item.discount}%</span>
+            <div data-id="${id}" class="button">В корзину</div>
+        </div>
+    </div>`);
+}
+drowItems(wishList);
+
+
+// создаем объект корзины 
+let shoppingCart = [];
+let emptyBasket = 'Ваша корзина пуста.';
+
+function basketItem(product, price, discount = 0) {
+    this.product = product;
+    this.price = price;
+    this.discount = discount;
+    this.finalPrice = function () {
+        if (this.discount != 0) {
+            return this.price - this.price * this.discount / 100;
+        } else {
+            return this.price;
+        }
+    }
+}
+
+// получаем итоговую сумму
+function totalSumm(shoppingCart) {
+    return shoppingCart.reduce(function (acc, price) {
+        return acc + price.finalPrice();
+    }, 0);
+}
+
+// так по приколу, правильные окончания
+function correctEnding() {
+    let corEnd = '';
+    if (shoppingCart.length == 1) {
+        corEnd = '';
+    } else if (shoppingCart.length > 1 && shoppingCart.length <= 4) {
+        corEnd = 'а';
+    } else {
+        corEnd = 'ов';
+    }
+    return corEnd;
+}
+
+// создаем отображение корзины
+function drowTotal(shoppingCart) {
+    const $basket = document.querySelector('#basket');
+    $basket.textContent = '';
+
+    if (shoppingCart == 0) {
+        $basket.insertAdjacentHTML('beforeend', `<div class="total">${emptyBasket}</div>`);
+    } else {
+        $basket.insertAdjacentHTML('beforeend',
+            `<div class="total">
+            <p>В корзине: ${shoppingCart.length} 
+            товар${correctEnding()} на сумму ${totalSumm(shoppingCart)} рублей.</p>
+            <a class="buy" href="#">Купить</a>
+        </div>`);
+    }
+}
+drowTotal(shoppingCart);
+
+// событие - добавление объекта в корзину
+$catalog.addEventListener('click', function (e) {
+    if (e.target.className === 'button') {
+        const id = Number(e.target.getAttribute('data-id'));
+        const choice = wishList[id];
+        shoppingCart.push(new basketItem(choice.product, choice.price, choice.discount));
+
+        drowTotal(shoppingCart);
+    }
+});
+
+// работаем с #popup
+const $popup = document.querySelector('#popup');
+
+$popup.addEventListener('click', function (e) {
+    $popup.style.display = 'none';
+});
+
+$catalog.addEventListener('click', function (e) {
+    if (e.target.tagName === 'IMG') {
+        $popup.textContent = '';
+        $popup.style.display = 'flex';
+        $popup.insertAdjacentHTML('beforeend',
+            `<img src="${e.target.getAttribute('src')}" class="scale">`);
+    }
+});
